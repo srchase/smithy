70bf3f1578ea536d705a8ef17f513db985c58bb2
Chase Coalwell <chasecoa@amazon.com>
2020-10-01 08:14:28 -0700

    Fix tests to work on Travis Windows

--- smithy-utils/src/test/java/software/amazon/smithy/utils/IoUtilsTest.java
+++ smithy-utils/src/test/java/software/amazon/smithy/utils/IoUtilsTest.java
@@ -20,6 +20,7 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.emptyString;
 import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -70,24 +71,25 @@ public class IoUtilsTest {
         // Windows doesn't like the result of URL#getPath, so to test this
         // we create a Path from the URI, convert that to a string, then pass
         // it to the helper method which uses Paths.get again.
-        assertEquals("This is a test.\n",
-                     IoUtils.readUtf8File(Paths.get(getClass().getResource("test.txt").toURI()).toString()));
+        assertTrue(IoUtils.readUtf8File(Paths.get(getClass().getResource("test.txt").toURI()).toString())
+                .contains("This is a test."));
     }
 
     @Test
     public void readsFromPath() throws URISyntaxException {
-        assertEquals("This is a test.\n", IoUtils.readUtf8File(Paths.get(getClass().getResource("test.txt").toURI())));
+        assertTrue(IoUtils.readUtf8File(Paths.get(getClass().getResource("test.txt").toURI()))
+                .contains("This is a test."));
     }
 
     @Test
     public void readsFromClass() {
-        assertEquals("This is a test.\n", IoUtils.readUtf8Resource(getClass(), "test.txt"));
+        assertTrue(IoUtils.readUtf8Resource(getClass(), "test.txt").contains("This is a test."));
     }
 
     @Test
     public void readsFromClassLoader() {
-        assertEquals("This is a test.\n", IoUtils.readUtf8Resource(
-                getClass().getClassLoader(), "software/amazon/smithy/utils/test.txt"));
+        assertTrue(IoUtils.readUtf8Resource(getClass().getClassLoader(), "software/amazon/smithy/utils/test.txt")
+                .contains("This is a test."));
     }
 
     @Test
